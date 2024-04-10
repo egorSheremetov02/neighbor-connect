008c8f356b978e94930615eedfa8a2e74c46e8f1
Nikita.Khramov <nikita.khramov@gmail.com>
2024-04-10 15:32:15 +0200

    fixes of modularity

--- server/incidents/incident_pb2.py
+++ server/incidents/incident_pb2.py
@@ -14,17 +14,17 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1b\x63ommon/proto/Incident.proto\x12\x08incident\"2\n\x0bGeoLocation\x12\x10\n\x08latitude\x18\x01 \x01(\x01\x12\x11\n\tlongitude\x18\x02 \x01(\x01\"\xc2\x01\n\x08Incident\x12\n\n\x02id\x18\x01 \x01(\t\x12\r\n\x05title\x18\x02 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x03 \x01(\t\x12$\n\x04type\x18\x04 \x01(\x0e\x32\x16.incident.IncidentType\x12$\n\x08severity\x18\x05 \x01(\x0e\x32\x12.incident.Severity\x12\'\n\x08location\x18\x06 \x01(\x0b\x32\x15.incident.GeoLocation\x12\x11\n\ttimestamp\x18\x07 \x01(\x03\"\x0e\n\x0cOfferRequest\"!\n\rOfferResponse\x12\x10\n\x08offerIds\x18\x01 \x03(\t\"\r\n\x0b\x43hatRequest\"\x1f\n\x0c\x43hatResponse\x12\x0f\n\x07\x63hatIds\x18\x01 \x03(\t\"%\n\x11\x43hatSearchRequest\x12\x10\n\x08\x63hatName\x18\x01 \x01(\t\"\x11\n\x0fIncidentRequest\"\'\n\x10IncidentResponse\x12\x13\n\x0bincidentIds\x18\x01 \x03(\t\"J\n\x12\x41\x64\x64IncidentRequest\x12\x0e\n\x06userId\x18\x01 \x01(\t\x12$\n\x08incident\x18\x02 \x01(\x0b\x32\x12.incident.Incident\")\n\x13\x41\x64\x64IncidentResponse\x12\x12\n\nincidentId\x18\x01 \x01(\t\"=\n\x17IncidentReactionRequest\x12\x0e\n\x06userId\x18\x01 \x01(\t\x12\x12\n\nreactionId\x18\x02 \x01(\x05\";\n\x18IncidentReactionResponse\x12\x0e\n\x06status\x18\x01 \x01(\t\x12\x0f\n\x07message\x18\x02 \x01(\t\"M\n\x18ProfileInfoUpdateRequest\x12\x10\n\x08userName\x18\x01 \x01(\t\x12\x10\n\x08password\x18\x02 \x01(\t\x12\r\n\x05\x65mail\x18\x03 \x01(\t\"<\n\x19ProfileInfoUpdateResponse\x12\x0e\n\x06status\x18\x01 \x01(\t\x12\x0f\n\x07message\x18\x02 \x01(\t\"7\n\x13\x43ommonGroupsRequest\x12\x0f\n\x07userId1\x18\x01 \x01(\t\x12\x0f\n\x07userId2\x18\x02 \x01(\t\"(\n\x14\x43ommonGroupsResponse\x12\x10\n\x08groupIds\x18\x01 \x03(\t\"\'\n\tDataChunk\x12\x0c\n\x04\x64\x61ta\x18\x01 \x01(\x0c\x12\x0c\n\x04size\x18\x02 \x01(\x05\" \n\x0cImageRequest\x12\x10\n\x08\x66ileName\x18\x01 \x01(\t\"$\n\x12ProfileInfoRequest\x12\x0e\n\x06userId\x18\x01 \x01(\t\"6\n\x13ProfileInfoResponse\x12\x10\n\x08userName\x18\x01 \x01(\t\x12\r\n\x05\x65mail\x18\x03 \x01(\t*H\n\x0cIncidentType\x12\x08\n\x04\x46IRE\x10\x00\x12\t\n\x05\x43RIME\x10\x01\x12\x0b\n\x07MEDICAL\x10\x02\x12\x0b\n\x07TRAFFIC\x10\x03\x12\t\n\x05OTHER\x10\x63*7\n\x08Severity\x12\x07\n\x03LOW\x10\x00\x12\n\n\x06MEDIUM\x10\x01\x12\x08\n\x04HIGH\x10\x02\x12\x0c\n\x08\x43RITICAL\x10\x03\x32M\n\x06Offers\x12\x43\n\x0egetLocalOffers\x12\x16.incident.OfferRequest\x1a\x17.incident.OfferResponse\"\x00\x32\x93\x03\n\x0cMapIncidents\x12@\n\rgetLocalChats\x12\x15.incident.ChatRequest\x1a\x16.incident.ChatResponse\"\x00\x12\x44\n\x0bsearchChats\x12\x1b.incident.ChatSearchRequest\x1a\x16.incident.ChatResponse\"\x00\x12L\n\x11getLocalIncidents\x12\x19.incident.IncidentRequest\x1a\x1a.incident.IncidentResponse\"\x00\x12L\n\x0b\x61\x64\x64Incident\x12\x1c.incident.AddIncidentRequest\x1a\x1d.incident.AddIncidentResponse\"\x00\x12_\n\x14sendIncidentReaction\x12!.incident.IncidentReactionRequest\x1a\".incident.IncidentReactionResponse\"\x00\x32\xbd\x01\n\x07Profile\x12^\n\x11updateProfileInfo\x12\".incident.ProfileInfoUpdateRequest\x1a#.incident.ProfileInfoUpdateResponse\"\x00\x12R\n\x0fgetCommonGroups\x12\x1d.incident.CommonGroupsRequest\x1a\x1e.incident.CommonGroupsResponse\"\x00\x32\x99\x01\n\tAuxiliary\x12;\n\x08getImage\x12\x16.incident.ImageRequest\x1a\x13.incident.DataChunk\"\x00\x30\x01\x12O\n\x0egetProfileInfo\x12\x1c.incident.ProfileInfoRequest\x1a\x1d.incident.ProfileInfoResponse\"\x00\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1b\x63ommon/proto/Incident.proto\x12\x08incident\"2\n\x0bGeoLocation\x12\x10\n\x08latitude\x18\x01 \x01(\x01\x12\x11\n\tlongitude\x18\x02 \x01(\x01\"\xc2\x01\n\x08Incident\x12\n\n\x02id\x18\x01 \x01(\t\x12\r\n\x05title\x18\x02 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x03 \x01(\t\x12$\n\x04type\x18\x04 \x01(\x0e\x32\x16.incident.IncidentType\x12$\n\x08severity\x18\x05 \x01(\x0e\x32\x12.incident.Severity\x12\'\n\x08location\x18\x06 \x01(\x0b\x32\x15.incident.GeoLocation\x12\x11\n\ttimestamp\x18\x07 \x01(\x03\"\x0e\n\x0cOfferRequest\"!\n\rOfferResponse\x12\x10\n\x08offerIds\x18\x01 \x03(\t\"\r\n\x0b\x43hatRequest\"\x1f\n\x0c\x43hatResponse\x12\x0f\n\x07\x63hatIds\x18\x01 \x03(\t\"%\n\x11\x43hatSearchRequest\x12\x10\n\x08\x63hatName\x18\x01 \x01(\t\"\x11\n\x0fIncidentRequest\"\'\n\x10IncidentResponse\x12\x13\n\x0bincidentIds\x18\x01 \x03(\t\"J\n\x12\x41\x64\x64IncidentRequest\x12\x0e\n\x06userId\x18\x01 \x01(\t\x12$\n\x08incident\x18\x02 \x01(\x0b\x32\x12.incident.Incident\")\n\x13\x41\x64\x64IncidentResponse\x12\x12\n\nincidentId\x18\x01 \x01(\t\"=\n\x17IncidentReactionRequest\x12\x0e\n\x06userId\x18\x01 \x01(\t\x12\x12\n\nreactionId\x18\x02 \x01(\x05\";\n\x18IncidentReactionResponse\x12\x0e\n\x06status\x18\x01 \x01(\t\x12\x0f\n\x07message\x18\x02 \x01(\t*H\n\x0cIncidentType\x12\x08\n\x04\x46IRE\x10\x00\x12\t\n\x05\x43RIME\x10\x01\x12\x0b\n\x07MEDICAL\x10\x02\x12\x0b\n\x07TRAFFIC\x10\x03\x12\t\n\x05OTHER\x10\x63*7\n\x08Severity\x12\x07\n\x03LOW\x10\x00\x12\n\n\x06MEDIUM\x10\x01\x12\x08\n\x04HIGH\x10\x02\x12\x0c\n\x08\x43RITICAL\x10\x03\x32M\n\x06Offers\x12\x43\n\x0egetLocalOffers\x12\x16.incident.OfferRequest\x1a\x17.incident.OfferResponse\"\x00\x32\x93\x03\n\x0cMapIncidents\x12@\n\rgetLocalChats\x12\x15.incident.ChatRequest\x1a\x16.incident.ChatResponse\"\x00\x12\x44\n\x0bsearchChats\x12\x1b.incident.ChatSearchRequest\x1a\x16.incident.ChatResponse\"\x00\x12L\n\x11getLocalIncidents\x12\x19.incident.IncidentRequest\x1a\x1a.incident.IncidentResponse\"\x00\x12L\n\x0b\x61\x64\x64Incident\x12\x1c.incident.AddIncidentRequest\x1a\x1d.incident.AddIncidentResponse\"\x00\x12_\n\x14sendIncidentReaction\x12!.incident.IncidentReactionRequest\x1a\".incident.IncidentReactionResponse\"\x00\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'common.proto.Incident_pb2', _globals)
 if _descriptor._USE_C_DESCRIPTORS == False:
   DESCRIPTOR._options = None
-  _globals['_INCIDENTTYPE']._serialized_start=1140
-  _globals['_INCIDENTTYPE']._serialized_end=1212
-  _globals['_SEVERITY']._serialized_start=1214
-  _globals['_SEVERITY']._serialized_end=1269
+  _globals['_INCIDENTTYPE']._serialized_start=731
+  _globals['_INCIDENTTYPE']._serialized_end=803
+  _globals['_SEVERITY']._serialized_start=805
+  _globals['_SEVERITY']._serialized_end=860
   _globals['_GEOLOCATION']._serialized_start=41
   _globals['_GEOLOCATION']._serialized_end=91
   _globals['_INCIDENT']._serialized_start=94
@@ -51,28 +51,8 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _globals['_INCIDENTREACTIONREQUEST']._serialized_end=668
   _globals['_INCIDENTREACTIONRESPONSE']._serialized_start=670
   _globals['_INCIDENTREACTIONRESPONSE']._serialized_end=729
-  _globals['_PROFILEINFOUPDATEREQUEST']._serialized_start=731
-  _globals['_PROFILEINFOUPDATEREQUEST']._serialized_end=808
-  _globals['_PROFILEINFOUPDATERESPONSE']._serialized_start=810
-  _globals['_PROFILEINFOUPDATERESPONSE']._serialized_end=870
-  _globals['_COMMONGROUPSREQUEST']._serialized_start=872
-  _globals['_COMMONGROUPSREQUEST']._serialized_end=927
-  _globals['_COMMONGROUPSRESPONSE']._serialized_start=929
-  _globals['_COMMONGROUPSRESPONSE']._serialized_end=969
-  _globals['_DATACHUNK']._serialized_start=971
-  _globals['_DATACHUNK']._serialized_end=1010
-  _globals['_IMAGEREQUEST']._serialized_start=1012
-  _globals['_IMAGEREQUEST']._serialized_end=1044
-  _globals['_PROFILEINFOREQUEST']._serialized_start=1046
-  _globals['_PROFILEINFOREQUEST']._serialized_end=1082
-  _globals['_PROFILEINFORESPONSE']._serialized_start=1084
-  _globals['_PROFILEINFORESPONSE']._serialized_end=1138
-  _globals['_OFFERS']._serialized_start=1271
-  _globals['_OFFERS']._serialized_end=1348
-  _globals['_MAPINCIDENTS']._serialized_start=1351
-  _globals['_MAPINCIDENTS']._serialized_end=1754
-  _globals['_PROFILE']._serialized_start=1757
-  _globals['_PROFILE']._serialized_end=1946
-  _globals['_AUXILIARY']._serialized_start=1949
-  _globals['_AUXILIARY']._serialized_end=2102
+  _globals['_OFFERS']._serialized_start=862
+  _globals['_OFFERS']._serialized_end=939
+  _globals['_MAPINCIDENTS']._serialized_start=942
+  _globals['_MAPINCIDENTS']._serialized_end=1345
 # @@protoc_insertion_point(module_scope)
--- server/incidents/run.py
+++ server/incidents/run.py
@@ -4,16 +4,12 @@ import incident_pb2_grpc
 
 from offers import OffersService
 from map_incidents import MapIncidentsService
-from profile_service import ProfileService
-from auxiliary import AuxiliaryService
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     incident_pb2_grpc.add_OffersServicer_to_server(OffersService(), server)
     incident_pb2_grpc.add_MapIncidentsServicer_to_server(MapIncidentsService(), server)
-    incident_pb2_grpc.add_ProfileServicer_to_server(ProfileService(), server)
-    incident_pb2_grpc.add_AuxiliaryServicer_to_server(AuxiliaryService(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
