ccfbf53a0a03b6c2aa963e88cd95bdc270c0c714
Yunus <tijaniyunus07@gmail.com>
2024-04-11 23:28:36 +0200

    implement add incidents

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,30 @@
-import React, { useState } from 'react';
-import LoginPage from './Pages/LoginPage/LoginPage';
-import Profile from './Pages/Profile/Profile';
-import Container from '@mui/material/Container';
-import Incidents from './Pages/Incidents';
-import Chats from './Pages/Chats/';
-import Offers from './Pages/Offers';
+import React, { useState } from "react";
+import LoginPage from "./Pages/LoginPage/LoginPage";
+import Profile from "./Pages/Profile/Profile";
+import Container from "@mui/material/Container";
+import Incidents from "./Pages/Incidents";
+import Chats from "./Pages/Chats/";
+import Offers from "./Pages/Offers";
 import { Button } from "@mui/material";
-
+import 'react-responsive-modal/styles.css';
+import AddIncidents from "./Pages/AddIncidents";
 
 const App = () => {
-  const [activeComponent, setActiveComponent] = useState('login');
+  const [activeComponent, setActiveComponent] = useState("login");
 
   const renderComponent = () => {
     switch (activeComponent) {
-      case 'login':
+      case "login":
         return <LoginPage />;
-      case 'profile':
+      case "profile":
         return <Profile />;
-      case 'chats':
+      case "chats":
         return <Chats />;
-      case 'incidents':
+      case "incidents":
         return <Incidents />;
-      case 'offers':
+      case "offers":
         return <Offers />;
+
       default:
         return null;
     }
@@ -33,14 +35,69 @@ const App = () => {
       <p className="mt-5 mr-5 sm:text-xl text-lg flex justify-end font-bold mb-4 mr-4 absolute top-0 right-0">
         Neighbor Connect
       </p>
-      <div style={{ position: 'relative' }}>
+      <div style={{ position: "relative" }}>
         <Container>
           <div className="mb-10 mt-10 flex justify-center items-end space-x-3 sm:space-x-2 sm:flex-wrap sm:space-y-2">
-            <Button variant="outlined" style={{backgroundColor: activeComponent === 'login' ? 'blue' : 'transparent', color: activeComponent === 'login' ? 'white' : '#1976d2' }} onClick={() => setActiveComponent('login')} sx={{ width: '100px', height: '40px' }}>Login</Button>
-            <Button variant="outlined" style={{backgroundColor: activeComponent === 'profile' ? 'blue' : 'transparent', color: activeComponent === 'profile' ? 'white' : '#1976d2' }} onClick={() => setActiveComponent('profile')} sx={{ width: '100px', height: '40px' }}>Profile</Button>
-            <Button variant="outlined" style={{backgroundColor: activeComponent === 'chats' ? 'blue' : 'transparent', color: activeComponent === 'chats' ? 'white' : '#1976d2' }} onClick={() => setActiveComponent('chats')} sx={{ width: '100px', height: '40px' }}>Chats</Button>
-            <Button variant="outlined" style={{backgroundColor: activeComponent === 'incidents' ? 'blue' : 'transparent', color: activeComponent === 'incidents' ? 'white' : '#1976d2' }} onClick={() => setActiveComponent('incidents')} sx={{ width: '100px', height: '40px' }}>Incidents</Button>
-            <Button variant="outlined" style={{backgroundColor: activeComponent === 'offers' ? 'blue' : 'transparent', color: activeComponent === 'offers' ? 'white' : '#1976d2' }} onClick={() => setActiveComponent('offers')} sx={{ width: '100px', height: '40px' }}>Offers</Button>
+            <Button
+              variant="outlined"
+              style={{
+                backgroundColor:
+                  activeComponent === "login" ? "blue" : "transparent",
+                color: activeComponent === "login" ? "white" : "#1976d2",
+              }}
+              onClick={() => setActiveComponent("login")}
+              sx={{ width: "100px", height: "40px" }}
+            >
+              Login
+            </Button>
+            <Button
+              variant="outlined"
+              style={{
+                backgroundColor:
+                  activeComponent === "profile" ? "blue" : "transparent",
+                color: activeComponent === "profile" ? "white" : "#1976d2",
+              }}
+              onClick={() => setActiveComponent("profile")}
+              sx={{ width: "100px", height: "40px" }}
+            >
+              Profile
+            </Button>
+            <Button
+              variant="outlined"
+              style={{
+                backgroundColor:
+                  activeComponent === "chats" ? "blue" : "transparent",
+                color: activeComponent === "chats" ? "white" : "#1976d2",
+              }}
+              onClick={() => setActiveComponent("chats")}
+              sx={{ width: "100px", height: "40px" }}
+            >
+              Chats
+            </Button>
+            <Button
+              variant="outlined"
+              style={{
+                backgroundColor:
+                  activeComponent === "incidents" ? "blue" : "transparent",
+                color: activeComponent === "incidents" ? "white" : "#1976d2",
+              }}
+              onClick={() => setActiveComponent("incidents")}
+              sx={{ width: "100px", height: "40px" }}
+            >
+              Incidents
+            </Button>
+            <Button
+              variant="outlined"
+              style={{
+                backgroundColor:
+                  activeComponent === "offers" ? "blue" : "transparent",
+                color: activeComponent === "offers" ? "white" : "#1976d2",
+              }}
+              onClick={() => setActiveComponent("offers")}
+              sx={{ width: "100px", height: "40px" }}
+            >
+              Offers
+            </Button>
           </div>
           {renderComponent()}
         </Container>
--- client/src/Pages/Incidents/index.jsx
+++ client/src/Pages/Incidents/index.jsx
@@ -1,21 +1,56 @@
-import React from 'react';
-import IncidentCard from './IncidentCard';
+import React, { useState } from "react";
+import IncidentCard from "./IncidentCard";
+import { Button } from "@mui/material";
+import AddIncidents from "../AddIncidents";
+import { Modal } from "react-responsive-modal";
 
 const Incidents = () => {
+  const [open, setOpen] = useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
   // Sample data for the cards
   const cardData = [
-    { id: 1, title: 'Incident 1' },
-    { id: 2, title: 'Incident 2' },
-    { id: 3, title: 'Incident 3' },
-    { id: 4, title: 'Incident 4' },
+    { id: 1, title: "Incident 1" },
+    { id: 2, title: "Incident 2" },
+    { id: 3, title: "Incident 3" },
+    { id: 4, title: "Incident 4" },
   ];
 
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', gap: '20px' }}>
-      {cardData.map((card) => (
-        <IncidentCard key={card.id} title={card.title} />
-      ))}
-    </div>
+    <>
+      <Modal open={open} onClose={onCloseModal} center>
+      <AddIncidents />
+
+      </Modal>
+
+      <div className="flex my-4 justify-end">
+        <Button
+          variant="outlined"
+          style={{
+            backgroundColor: "transparent",
+            color: "#1976d2",
+          }}
+          onClick={onOpenModal}
+          sx={{ width: "150px", height: "40px" }}
+        >
+          Add Incidents
+        </Button>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+          gap: "20px",
+        }}
+      >
+        {cardData.map((card) => (
+          <IncidentCard key={card.id} title={card.title} />
+        ))}
+      </div>
+    </>
   );
 };
 
