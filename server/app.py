f55da29ceaa9551b8139ad6e1b6609cb50c1d63c
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2024-04-12 01:29:41 +0200

    ggwp

--- server/app.py
+++ server/app.py
@@ -1,2 +1,11 @@
+from db.tables import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db.requests import Database
+
 if __name__ == '__main__':
     print('Starting server.')
+    conn_url = 'postgresql+psycopg2://postgres:1234@nc_db/neighbourhood_db'
+    db = Database(conn_url)
+    print(db.get_users())
+    print('Server started.')
