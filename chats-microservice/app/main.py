8f14a580f91be9852b96161d1b02139af38dd3e1
Anton Naumtsev <naumtsevanton@mail.ru>
2024-04-24 17:11:39 +0200

    update models

--- chats-microservice/app/main.py
+++ chats-microservice/app/main.py
@@ -1,30 +1,35 @@
 from typing import Union
 
 from fastapi import FastAPI
+from .models import (CreateChatRequest, CreateChatResponse,
+                    EditChatDataRequest, EditChatDataResponse,
+                    DeleteChatRequest, DeleteChatResponse,
+                    SendMessageRequest, SendMessageResponse,
+                    ListMessagesRequest, ListMessagesResponse)
 
 app = FastAPI()
 
 
 @app.post("/chats")
-def create_chat():
+def create_chat(request: CreateChatRequest) -> CreateChatResponse:
     return
 
 
 @app.put("/chats")
-def edit_chat_data():
+def edit_chat_data(request: EditChatDataRequest) -> EditChatDataResponse:
     return
 
 
 @app.delete("/chats")
-def delete_chat():
+def delete_chat(request: DeleteChatRequest) -> DeleteChatResponse:
     return
 
 
 @app.post("/chats/{chat_id}")
-def send_message(chat_id: int):
+def send_message(chat_id: int, request: SendMessageRequest) -> SendMessageResponse:
     return
 
 
 @app.get("/chats/{chat_id}")
-def list_messages(chat_id: int):
+def list_messages(chat_id: int, request: ListMessagesRequest) -> ListMessagesResponse:
     return
