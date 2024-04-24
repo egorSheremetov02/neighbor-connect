d2467d78fcb56291427a2738090ecacfd37ea677
Sergei Sysoev <Sergey.Sysoev@jetbrains.com>
2024-04-25 02:35:09 +0300

    Create database on start

--- chats-microservice/app/main.py
+++ chats-microservice/app/main.py
@@ -1,6 +1,14 @@
+import uvicorn
 from fastapi import FastAPI
 from app.api.main import api_router
+# importing db_models initializes all classes, so that when we `create_all`, it creates all tables
+from app.db_models.chats import *
+from app.core.db import DBBase, engine
 
-
+DBBase.metadata.create_all(engine)
 app = FastAPI()
 app.include_router(api_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info")
