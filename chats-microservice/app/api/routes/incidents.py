5613a60f9cab17b678ec72b1e171972ec2e2a2a2
klimoza <i220303@mail.ru>
2024-04-26 01:25:47 +0200

    Add incident create/edit/delete endpoints

--- chats-microservice/app/api/routes/incidents.py
+++ chats-microservice/app/api/routes/incidents.py
@@ -7,18 +7,39 @@ from app.api_models.chats import (ListIncidentsRequest, ListIncidentsResponse,
                                   AuthorizeIncidentResponse)
 import logging, sqlalchemy
 
-from app.db_models.chats import Incident
+from app.db_models.chats import User
+from app.db_models.incidents import Incident
 from app.core.db import SessionLocal
-from app.api.util import get_current_user_id, validate_tags
+from app.api.util import get_current_user_id, validate_tags, check_user_account_status
 from fastapi import HTTPException
 
 incidents_router = APIRouter()
 logger = logging.getLogger(__name__)
 
-
 @incidents_router.post("/")
 def create_incident(request: CreateIncidentRequest) -> CreateIncidentResponse:
-    pass 
+    sender_id = get_current_user_id()
+
+    check_user_account_status(sender_id)
+
+    if len(request.title) == 0:
+        raise HTTPException(400, f'Incident title is missing')
+    if len(request.description) == 0:
+        raise HTTPException(400, f'Incident description is missing')
+    if sender_id != request.author_id:
+        raise HTTPException(400, f'Incident creator should be the same as the author')
+
+    with SessionLocal() as session:
+        with session.begin():
+            author = session.query(User).filter_by(id=request.author_id).first()
+
+            if author is None:
+                raise HTTPException(400, f'User with id {author} does not exist')
+
+            incident = Incident(title=request.title, description=request.description, author_id=request.author_id, author=author)
+            session.add(author)
+
+        return CreateIncidentResponse(id=incident.id)
 
 
 @incidents_router.get("/")
@@ -26,14 +47,54 @@ def list_incidents(request: ListIncidentsRequest) -> ListIncidentsResponse:
     pass
 
 
-@incidents_router.delete("/")
-def delete_incident(request: DeleteIncidentRequest) -> DeleteIncidentResponse:
-    pass
+@incidents_router.delete("/{incident_id}")
+def delete_incident(incident_id: int, request: DeleteIncidentRequest) -> DeleteIncidentResponse:
+    sender_id = get_current_user_id()
+
+    check_user_account_status(sender_id)
+
+    with SessionLocal() as session:
+        with session.begin():
+            incident = session.query(Incident).filter_by(id=incident_id).first()
+
+            if incident is None:
+                raise HTTPException(404, f'Incident with id {incident_id} does not exist')
+            
+            if incident.author_id != sender_id:
+                raise HTTPException(403, f'User doesn\'t have permission to delete this incident')
+
+            session.delete(incident)
+
+        return DeleteIncidentResponse()
 
 
-@incidents_router.put("/")
-def edit_incident_data(request: EditIncidentDataRequest) -> EditIncidentDataResponse:
-    pass
+@incidents_router.put("/{incident_id}")
+def edit_incident_data(incident_id: int, request: EditIncidentDataRequest) -> EditIncidentDataResponse:
+    sender_id = get_current_user_id()
+
+    check_user_account_status(sender_id)
+
+    if len(request.title) == 0:
+        raise HTTPException(400, f'Incident title is missing')
+    if len(request.description) == 0:
+        raise HTTPException(400, f'Incident description is missing')
+    if sender_id != request.author_id:
+        raise HTTPException(400, f'Incident creator should be the same as the author')
+
+    with SessionLocal() as session:
+        with session.begin():
+            incident = session.query(Incident).filter_by(id=incident_id).first()
+
+            if incident is None:
+                raise HTTPException(404, f'Incident with id {incident_id} does not exist')
+            
+            if incident.author_id != sender_id:
+                raise HTTPException(403, f'User doesn\'t have permission to delete this incident')
+
+            incident.title = request.title
+            incident.description = request.description
+
+        return EditIncidentDataResponse()
 
 
 @incidents_router.post("/authorize")
