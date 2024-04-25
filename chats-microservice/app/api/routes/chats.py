921bbd9cf620609094d3a6dd754e93e7f1059abd
Sergei Sysoev <Sergey.Sysoev@jetbrains.com>
2024-04-25 03:32:03 +0300

    create_chat is most of the way there

--- chats-microservice/app/api/routes/chats.py
+++ chats-microservice/app/api/routes/chats.py
@@ -6,13 +6,31 @@ from app.api_models.chats import (CreateChatRequest, CreateChatResponse,
                                   DeleteChatRequest, DeleteChatResponse,
                                   SendMessageRequest, SendMessageResponse,
                                   ListMessagesRequest, ListMessagesResponse)
+from app.db_models.chats import Chat, Tag
+from app.core.db import SessionLocal
 
 chats_router = APIRouter()
 
 
 @chats_router.post("/")
 def create_chat(request: CreateChatRequest) -> CreateChatResponse:
-    return
+    with SessionLocal() as session:
+        with session.begin():
+            tags = []
+            for tag in request.tags:
+                tag_object = session.query(Tag).filter_by(name=tag).first()
+                if not tag_object:
+                    tag_object = Tag(name=tag)
+                    session.add(tag_object)
+                tags.append(tag_object)
+
+            chat = Chat(name=request.name,
+                        description=request.description,
+                        tags=tags
+                        # image_url=request.image_url TODO
+                        )
+            session.add(chat)
+        return CreateChatResponse(chat_id=chat.id)
 
 
 @chats_router.put("/")
--- chats-microservice/app/db_models/chats.py
+++ chats-microservice/app/db_models/chats.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+from typing import List
 
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Table
+from sqlalchemy.orm import relationship, mapped_column, Mapped
 
 from app.core.db import DBBase
 
@@ -19,17 +21,27 @@ class Message(DBBase):
     created_at: datetime = Column(DateTime, default=datetime.utcnow)
 
 
+tag_chat_association_table = Table(
+    "tag_chat_association_table",
+    DBBase.metadata,
+    Column("tag_name", ForeignKey("tags.name")),
+    Column("chat_id", ForeignKey("chats.id")),
+)
+
+
 class Tag(DBBase):
     __tablename__ = "tags"
-    name = Column(String, primary_key=True)
+
+    name: Mapped[str] = mapped_column(primary_key=True)
+    # name = Column(String, primary_key=True)
 
 
 class Chat(DBBase):
     __tablename__ = "chats"
 
-    id = Column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     name = Column(String, nullable=False)
     description = Column(String, nullable=True)
-    # tags: list[str] = Column()
-    # image_id: int | None = None
+    tags: Mapped[List[Tag]] = relationship(secondary=tag_chat_association_table)
+    # image_id = Column(Integer, nullable=True) TODO
     # users: list[int]
