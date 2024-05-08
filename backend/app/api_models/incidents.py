09f65218326531b1fb24e8cbebe06f4c56d9d276
Nikita.Khramov <nikita.khramov@gmail.com>
2024-05-08 20:41:51 +0200

    updated incidents

--- backend/app/api_models/incidents.py
+++ backend/app/api_models/incidents.py
@@ -3,13 +3,15 @@ from datetime import datetime
 
 
 class Incident(BaseModel):
-    id: int 
-    title: str 
-    description: str 
-    author_id: int 
-    status: str 
-    created_at: datetime 
+    id: int
+    title: str
+    description: str
+    author_id: int
+    status: str
+    created_at: datetime
     updated_at: datetime
+    location: str | None = None
+    image_id: int | None = None
 
 
 class ListIncidentsResponse(BaseModel):
@@ -18,11 +20,15 @@ class ListIncidentsResponse(BaseModel):
 
 class CreateIncidentRequest(BaseModel):
     title: str
-    description: str 
+    description: str
+    created_at: datetime
+    updated_at: datetime | None = None
+    location: str
+    image_id: int | None = None
 
 
 class CreateIncidentResponse(BaseModel):
-    id: int 
+    id: int
 
 
 class DeleteIncidentRequest(BaseModel):
@@ -35,7 +41,11 @@ class DeleteIncidentResponse(BaseModel):
 
 class EditIncidentDataRequest(BaseModel):
     title: str
-    description: str 
+    description: str
+    location: str
+    image_id: int | None = None
+    updated_at: datetime
+
 
 class EditIncidentDataResponse(BaseModel):
     pass
