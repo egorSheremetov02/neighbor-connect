edcd89a748096efc7a739a11db8b24cf069daa05
GlebSolovev <lionyoung@mail.ru>
2024-10-09 00:12:30 +0200

    Format init_db.py

--- backend/app/init_db.py
+++ backend/app/init_db.py
@@ -2,6 +2,7 @@ from app.db_models.chats import Chat, User
 from app.core.db import SessionLocal
 from app.api.util import get_password_hash
 
+
 def init_db():
     """
     Initializes the database with a test user.
@@ -12,15 +13,15 @@ def init_db():
         password_hashed = get_password_hash("123456")
         # This user is just for health check purposes
         test_user = User(
-                    id=0, 
-                    name='Admin Adminovich',
-                    email="admin@capstone.com",
-                    login="admin",
-                    password_hashed=password_hashed,
-                    birthday="2021-01-01",
-                    permanent_address="CUB Bremen",
-                    image_id=None
-                )
+            id=0,
+            name="Admin Adminovich",
+            email="admin@capstone.com",
+            login="admin",
+            password_hashed=password_hashed,
+            birthday="2021-01-01",
+            permanent_address="CUB Bremen",
+            image_id=None,
+        )
         session.merge(test_user)
         session.flush()
         session.merge(
