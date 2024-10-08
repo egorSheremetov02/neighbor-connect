520b3d2fafac9be81ffd80374c53a858346c747c
GlebSolovev <lionyoung@mail.ru>
2024-10-08 19:29:50 +0200

    Format chats.py

--- backend/app/db_models/chats.py
+++ backend/app/db_models/chats.py
@@ -25,7 +25,7 @@ class Message(DBBase):
     content: Mapped[str] = mapped_column(nullable=False)
     image_id: Mapped[int | None] = mapped_column(ForeignKey("images.id"), default=None)
     author_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    author: Mapped[User] = relationship()  
+    author: Mapped[User] = relationship()
     created_at: Mapped[datetime] = mapped_column(default=datetime.now)
     chat_id: Mapped[int] = mapped_column(ForeignKey("chats.id", ondelete="CASCADE"))
 
@@ -39,21 +39,21 @@ _chats_to_tags_assoc_table = Table(
     "chats_tags",
     DBBase.metadata,
     Column("chat_id", ForeignKey("chats.id", ondelete="CASCADE")),
-    Column("tag_name", ForeignKey("tags.name", ondelete="CASCADE"))
+    Column("tag_name", ForeignKey("tags.name", ondelete="CASCADE")),
 )
 
 _chats_to_users_assoc_table = Table(
     "chats_users",
     DBBase.metadata,
     Column("chat_id", ForeignKey("chats.id", ondelete="CASCADE")),
-    Column("user_id", ForeignKey("users.id", ondelete="CASCADE"))
+    Column("user_id", ForeignKey("users.id", ondelete="CASCADE")),
 )
 
 _chats_to_admins_assoc_table = Table(
     "chats_admins",
     DBBase.metadata,
     Column("chat_id", ForeignKey("chats.id", ondelete="CASCADE")),
-    Column("admin_id", ForeignKey("users.id", ondelete="CASCADE"))
+    Column("admin_id", ForeignKey("users.id", ondelete="CASCADE")),
 )
 
 
@@ -62,11 +62,19 @@ class Chat(DBBase):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column()
     description: Mapped[str | None] = mapped_column()
-    tags: Mapped[List[Tag]] = relationship(secondary=_chats_to_tags_assoc_table, cascade="all, delete")
+    tags: Mapped[List[Tag]] = relationship(
+        secondary=_chats_to_tags_assoc_table, cascade="all, delete"
+    )
     image_id: Mapped[int | None] = mapped_column(ForeignKey("images.id"), default=None)
-    users: Mapped[List[User]] = relationship(secondary=_chats_to_users_assoc_table, cascade="all, delete")
-    admins: Mapped[List[User]] = relationship(secondary=_chats_to_admins_assoc_table, cascade="all, delete")
-    messages: Mapped[List[Message]] = relationship(order_by="desc(Message.created_at)", cascade="all, delete")
+    users: Mapped[List[User]] = relationship(
+        secondary=_chats_to_users_assoc_table, cascade="all, delete"
+    )
+    admins: Mapped[List[User]] = relationship(
+        secondary=_chats_to_admins_assoc_table, cascade="all, delete"
+    )
+    messages: Mapped[List[Message]] = relationship(
+        order_by="desc(Message.created_at)", cascade="all, delete"
+    )
 
 
 class Image(DBBase):
