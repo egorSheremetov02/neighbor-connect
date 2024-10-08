be43a926784c1639d95b9e679e0cd478f8493346
Ahmed Elsayed <66392241+AhmedElsayed200@users.noreply.github.com>
2024-10-08 23:40:54 +0200

    theme and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,47 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './Pages/Home';
-import LoginForm from './Components/LoginForm/LoginForm';
-import RegistrationForm from './Components/RegistrationForm/RegistrationForm';
-import { AuthProvider } from "./auth/index"; 
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Neighbors from "./Pages/Neighbors";
+import Profile from "./Pages/Profile";
+import Wrapper from "./Components/Wrapper";
+import "@fontsource/inter";
 
 const App = () => {
-  console.log(sessionStorage.getItem('token'))
+  console.log(sessionStorage.getItem("token"));
   return (
-    <AuthProvider>
-      <Router basename="/">
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/register" element={<RegistrationForm />} />
-          </Routes>
-      </Router>
-    </AuthProvider>
+    <Router basename="/">
+      <Routes>
+        <Route
+          path="/home"
+          exact
+          element={
+            <Wrapper>
+              <Home />
+            </Wrapper>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/neighbors"
+          element={
+            <Wrapper>
+              <Neighbors />
+            </Wrapper>
+          }
+        />
+        <Route
+          path="/neighbors/:id"
+          element={
+            <Wrapper>
+              <Profile />
+            </Wrapper>
+          }
+        />
+      </Routes>
+    </Router>
   );
 };
 
