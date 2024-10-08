594a47a113276d1408a8ef68b59589720f027e27
Ahmed Elsayed <66392241+AhmedElsayed200@users.noreply.github.com>
2024-10-08 23:40:54 +0200

    theme and routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+import "./index.css";
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const theme = createTheme({
+  typography: {
+    fontFamily: `"Inter", "sans-serif"`, // Global font setting
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
-)
+  </ThemeProvider>
+);
