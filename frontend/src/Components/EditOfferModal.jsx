6e49c5bcc002d5b0dba372427c646797c1bff596
Ahmed Elsayed <66392241+AhmedElsayed200@users.noreply.github.com>
2024-10-08 23:44:29 +0200

    edit posts modal

---
+++ frontend/src/Components/EditOfferModal.jsx
@@ -0,0 +1,184 @@
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  Button,
+} from "@mui/material";
+
+const EditOfferModal = ({ open, onClose, offer, onEditSuccess }) => {
+  const [formData, setFormData] = useState({
+    offer_id: offer.id,
+    title: offer.title,
+    description: offer.description,
+    price: offer.price,
+    product: offer.product,
+    date: new Date(),
+    image_id: 0,
+  });
+  const token = sessionStorage.getItem("TOKEN");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/offers/", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token.substring(1, token.length - 1),
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to update offer");
+      }
+
+      onEditSuccess();
+      onClose();
+    } catch (error) {
+      console.error("Error editing offer:", error);
+    }
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      style={{ backdropFilter: "blur(5px)" }}
+    >
+      <DialogTitle>Edit Offer</DialogTitle>
+      <DialogContent>
+        <DialogContentText>Update the offer details below.</DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
+          name="title"
+          label="Title"
+          fullWidth
+          value={formData.title}
+          onChange={handleChange}
+          required
+          InputProps={{
+            sx: {
+              borderRadius: "8px",
+              backgroundColor: "#f9f9f9",
+              height: "40px",
+              fontSize: "14px",
+            },
+          }}
+          InputLabelProps={{
+            sx: {
+              fontSize: "14px",
+            },
+          }}
+        />
+        <TextField
+          margin="dense"
+          name="description"
+          label="Description"
+          fullWidth
+          multiline
+          value={formData.description}
+          onChange={handleChange}
+          required
+          InputProps={{
+            sx: {
+              borderRadius: "8px",
+              backgroundColor: "#f9f9f9",
+              height: "40px",
+              fontSize: "14px",
+            },
+          }}
+          InputLabelProps={{
+            sx: {
+              fontSize: "14px",
+            },
+          }}
+        />
+        <TextField
+          margin="dense"
+          name="price"
+          label="Price"
+          type="number"
+          fullWidth
+          value={formData.price}
+          onChange={handleChange}
+          required
+          InputProps={{
+            sx: {
+              borderRadius: "8px",
+              backgroundColor: "#f9f9f9",
+              height: "40px",
+              fontSize: "14px",
+            },
+          }}
+          InputLabelProps={{
+            sx: {
+              fontSize: "14px",
+            },
+          }}
+        />
+        <TextField
+          margin="dense"
+          name="product"
+          label="Product"
+          fullWidth
+          value={formData.product}
+          onChange={handleChange}
+          required
+          InputProps={{
+            sx: {
+              borderRadius: "8px",
+              backgroundColor: "#f9f9f9",
+              height: "40px",
+              fontSize: "14px",
+            },
+          }}
+          InputLabelProps={{
+            sx: {
+              fontSize: "14px",
+            },
+          }}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={onClose}
+          sx={{
+            color: "black",
+            background: "#e2e2e2",
+            fontSize: "10px",
+            "&:hover": {
+              background: "#e2e2e2",
+            },
+          }}
+        >
+          Cancel
+        </Button>
+        <Button
+          onClick={handleSubmit}
+          sx={{
+            color: "black",
+            background: "#e2e2e2",
+            fontSize: "10px",
+            "&:hover": {
+              background: "#e2e2e2",
+            },
+          }}
+        >
+          Save
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default EditOfferModal;
