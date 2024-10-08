5661d4551de201dafa4bd2d60e1999288478bc01
Ahmed Elsayed <66392241+AhmedElsayed200@users.noreply.github.com>
2024-10-08 23:41:20 +0200

    constants and functions

---
+++ frontend/src/assets/functions.js
@@ -0,0 +1,16 @@
+export function formatDate(isoDateString) {
+  const date = new Date(isoDateString);
+
+  // Use toLocaleString for customizable output
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: true,
+  };
+
+  return date.toLocaleString("en-US", options);
+}
